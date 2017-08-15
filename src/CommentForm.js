311105aa9228b6b914339aed2d75407c3fa5df77
13037522 <30622652+13037522@users.noreply.github.com>
2017-08-15 20:12:52 +1000

    Validated version

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,5 +1,7 @@
 //CommentForm.js
+
 import React, { Component } from 'react';
+import Validation from 'react-validation';
 import style from './style';
 class CommentForm extends Component {
  constructor(props) {
@@ -12,6 +14,7 @@ class CommentForm extends Component {
    {synopsis: 'Synopsis'},
  ];
 
+
  this.state = { title: '', releasedate: '', duration: '', genre: '', synopsis: ''};
  this.handleTitleChange = this.handleTitleChange.bind(this);
  this.handleReleaseDateChange = this.handleReleaseDateChange.bind(this);
@@ -20,7 +23,65 @@ class CommentForm extends Component {
  this.handleSynopsisChange = this.handleSynopsisChange.bind(this);
  this.handleSubmit = this.handleSubmit.bind(this);
  }
+ import validator from 'validator';
 
+ // Use Object.assign or any similar API to merge a rules
+ // NOTE: IE10 doesn't have Object.assign API natively. Use polyfill/babel plugin.
+ Object.assign(Validation.rules, {
+     // Key name maps the rule
+     required: {
+         // Function to validate value
+         // NOTE: value might be a number -> force to string
+         rule: value => {
+             return value.toString().trim();
+         },
+         // Function to return hint
+         // You may use current value to inject it in some way to the hint
+         hint: value => {
+             return <span className='form-error is-visible'>Required</span>
+         }
+     },
+     email: {
+         // Example usage with external 'validator'
+         rule: value => {
+             return validator.isEmail(value);
+         },
+         hint: value => {
+             return <span className='form-error is-visible'>{value} isnt an Email.</span>
+         }
+     },
+     // This example shows a way to handle common task - compare two fields for equality
+     password: {
+         // rule function can accept argument:
+         // components - components registered to Form mapped by name
+         rule: (value, components) => {
+             const password = components.password.state;
+             const passwordConfirm = components.passwordConfirm.state;
+             const isBothUsed = password
+                 && passwordConfirm
+                 && password.isUsed
+                 && passwordConfirm.isUsed;
+             const isBothChanged = isBothUsed && password.isChanged && passwordConfirm.isChanged;
+
+             if (!isBothUsed || !isBothChanged) {
+                 return true;
+             }
+
+             return password.value === passwordConfirm.value;
+         },
+         hint: () => <span className="form-error is-visible">Passwords should be equal.</span>
+     },
+     // Define API rule to show hint after API error response
+     api: {
+         // We don't need the rule here because we will call the 'showError' method by hand on API error
+         hint: value => (
+             <button
+                 className="form-error is-visible"
+             >
+                 API Error on "{value}" value. Focus to hide.
+             </button>
+         )
+     }
  handleTitleChange(e) {
  this.setState({ title: e.target.value });
  }
@@ -45,7 +106,7 @@ class CommentForm extends Component {
  render() {
  return (
  <form style={ style.commentForm }
- onSubmit={ this.handleSubmit }>
+       onSubmit={ this.handleSubmit }>
 <h3>Movie Title:</h3>
  <input
  type='title'
